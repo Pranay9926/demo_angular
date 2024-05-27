4991e42867baee1741ee8ca49bd5ff7847f5d805
Pranay Shukla <pranayshukla@Administrators-iMac-2.local>
2024-05-27 19:17:37 +0530

    Created Header and updated login page

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  Logo = "../../../assets/hiteshiLogo.png"
+  isOpen: boolean = false;
+
+  handleClick() {
+    this.isOpen = !this.isOpen
+  }
 
 }
