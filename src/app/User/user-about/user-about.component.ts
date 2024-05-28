911e7f37f2c52659b59e186b6b02e74c78ce9235
Pranay Shukla <pranayshukla@Administrators-iMac-2.local>
2024-05-28 19:14:40 +0530

    Implement pipes

--- src/app/User/user-about/user-about.component.ts
+++ src/app/User/user-about/user-about.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-about',
   standalone: true,
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DecimalPipe, DatePipe],
   templateUrl: './user-about.component.html',
   styleUrl: './user-about.component.scss'
 })
 export class UserAboutComponent {
+
+  currentDate = new Date
+
+
+  students = [
+    { id: 1, name: 'Pranay', gender: 'Male' },
+    { id: 2, name: 'Aaditya', gender: 'Male' },
+    { id: 3, name: 'shurti', gender: 'Female' },
+    { id: 4, name: 'Shubham', gender: 'Male' },
+  ]
 
 }
